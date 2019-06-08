eb9d5299444ab80713656a8fef608e7c88efe483
Gabriel Laliberté <gabriel.laliberte@ticketmaster.com>
2019-06-08 13:19:18 -0400

    pokemon icon in title

--- src/components/pokemon/PokemonTitle.tsx
+++ src/components/pokemon/PokemonTitle.tsx
@@ -11,10 +11,17 @@ export class PokemonTitle extends React.Component<PokemonTitleProps, any> {
   render() {
     return (
       <section className='pokemon-title hero is-info'>
-        <div className='hero-body'>
-          <h1 className='pokemon-title__text title has-text-left'>
-            #{this.props.nationalNumber} {this.props.name}
-          </h1>
+        <div className='hero-body level'>
+          <div className='level-left'>
+            <h1 className='pokemon-title__text title level-item has-text-left'>
+              #{this.props.nationalNumber} {this.props.name}
+            </h1>
+            <img
+              src={process.env.PUBLIC_URL + '/icons/pokemons/' + this.props.name + '.png'}
+              className='pokemon-title__icon level-item'
+              alt=''
+            />
+          </div>
         </div>
       </section>
     )
