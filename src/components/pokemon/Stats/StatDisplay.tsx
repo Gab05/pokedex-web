c60bb4bb72b4a457573106d07c90df85158e0e91
Gabriel Laliberté <gabriel.laliberte@ticketmaster.com>
2019-06-14 00:14:52 -0400

    added swag to base stats display

--- src/components/pokemon/Stats/StatDisplay.tsx
+++ src/components/pokemon/Stats/StatDisplay.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './StatDisplay.css'
 
 interface StatDisplayProps {
   name: string
@@ -17,19 +18,21 @@ export class StatDisplay extends React.Component<StatDisplayProps, any> {
 
   render() {
     return (
-      <div className='level'>
-        <h2 className='level-item'>{this.props.name}</h2>
-        <progress
-          className={'progress ' + this.calculateColor(this.props.value, 200)}
-          value={this.props.value}
-          max={200}
-        />
-        <h2 className='level-item'>{this.props.value}</h2>
+      <div className='level stat__display'>
+          <h2 className='stat__name level-item has-text-left'>{this.props.name}</h2>
+          <h2 className='stat__value level-item has-text-right'>{this.props.value}</h2>
+          <progress
+            className={'progress level-item stat__bar box ' + this.calculateColor(this.props.value, 200)}
+            value={this.props.value}
+            max={200}
+          />
       </div>
     )
   }
 
   private calculateColor = (actual: number, max: number): string => {
-    return this.COLOR_CLASS_MAP[Math.floor((actual/max) * this.COLOR_CLASS_MAP.length)]
+    return actual <= max
+      ? this.COLOR_CLASS_MAP[Math.floor((actual/max) * this.COLOR_CLASS_MAP.length)]
+      : 'is-link'
   }
 }
